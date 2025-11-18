255869e8486b7041deae70f5222c42e6dadbe5f8
jdudzinska625 <jdudzinska625@gmail.com>
2025-11-18 23:25:18 +0100

    Remove unused code

--- tuning/grid_search.py
+++
@@ -1,7 +0,0 @@
-from sklearn.model_selection import GridSearchCV
-
-
-def run_grid_search(model_cls, param_space, X_train, y_train, metric):
-    search = GridSearchCV(model_cls(), param_space, scoring=metric, cv=3, n_jobs=-1)
-    search.fit(X_train, y_train)
-    return search.best_params_, search.best_score_
--- tuning/random_search.py
+++
@@ -1,9 +0,0 @@
-from sklearn.model_selection import RandomizedSearchCV
-
-
-def run_random_search(model_cls, param_space, X_train, y_train, metric):
-    search = RandomizedSearchCV(
-        model_cls(), param_space, n_iter=20, scoring=metric, cv=3, n_jobs=-1
-    )
-    search.fit(X_train, y_train)
-    return search.best_params_, search.best_score_
